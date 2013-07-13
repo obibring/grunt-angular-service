d75dd9d5cd8664c9e7d72540a263181a35d6b6d3
Orr Bibring <orrbibring@Orrs-MacBook-Pro-2.local>
2013-07-13 00:51:24 -0400

    task working

--- tasks/angular_modularize.js
+++ tasks/angular_modularize.js
@@ -13,28 +13,30 @@
  */
 
 'use strict';
-_ = require('underscore');
+var _ = require('underscore');
 
-// Template that wraps libraries in angular module definition.
-var template = _.template("(function(angular){angular.module('<%= module %>')" +
-  ".factory('<%= factory %>', function() {" +
-  "  function temp() { " +
-  "    <%= src %> " +
-  "  } " +
-  "  var lib = new temp(); " +
-  "  var properties = [];
-  "  for (var key in lib) if (lib.hasOwnProperty(key)) properties.push(key); " +
-  "  if (properties.length === 1)" +
-  "    return lib[properties.pop()]; " +
-  "  else " +
-  "    return lib; " +
-  "})(window.angular)");
+// Template that wraps libraries in angular factory definition.
+var template = _.template("(function(angular) {\n"+
+  "  angular.module('<%= module %>')\n\t" +
+  "    .factory('<%= factory %>', function() {\n\t\t" +
+  "      function temp() {\n\t\t\t" +
+  "        <%= src %>\n\t\t\t" +
+  "      }\n\t\t" +
+  "      var lib = new temp();\n" +
+  "      var properties = [];\n" +
+  "      for (var key in lib) { \n" +
+  "        if (lib.hasOwnProperty(key)) properties.push(key);\n" +
+  "      }\n" +
+  "      if (properties.length === 1) {\n" +
+  "        return lib[properties.pop()];\n" +
+  "       } else {\n" +
+  "         return lib;\n" +
+  "       } " +
+  "     });\n" +
+  "  })(window.angular);");
 
 module.exports = function(grunt) {
 
-  var warn = (function (message) {
-    grunt.log.warn(this.nameArgs + ": " + message);
-  }).bind(this);
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
@@ -43,6 +45,14 @@ module.exports = function(grunt) {
     'angular_modularize',
     'Convert Javascript libraries to AngularJS modules', function() {
 
+        var warn = function (message) {
+          grunt.log.warn(this.name + ": " + message);
+        };
+
+        var log = function (message) {
+          grunt.log.writeln(this.name + ": " + message);
+        }.bind(this);
+
       // Merge task-specific and/or target-specific options with these defaults.
       var options = this.options({
         punctuation: '.',
@@ -50,11 +60,13 @@ module.exports = function(grunt) {
       });
 
       // Must provide name of angular module to be created.
-      this.requiresConfig('module');
-      this.requiresConfig('factory');
+      //this.requiresConfig(this.name+'.module');
+      //this.requiresConfig(this.name+'.factory');
 
-      var module = this.options.module;
-      var factory = this.options.factory;
+      var module = this.data.module;
+      var factory = this.data.factory;
+
+      console.log(this.files);
 
       // Iterate over all specified file groups.
       this.files.forEach(function(f) {
@@ -81,7 +93,7 @@ module.exports = function(grunt) {
           grunt.file.write(f.dest, angularized);
 
           // Print a success message.
-          grunt.log.writeln('File "' + f.dest + '" created.');
+          log('File "' + f.dest + '" created.');
         });
 
     });
